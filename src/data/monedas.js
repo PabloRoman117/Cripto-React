94e7ce360945b57279f5396773c5ce2932dc02a8
Pablo Roman <pablorcb117@gmail.com>
2022-01-08 19:46:49 -0600

    primera version

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    {id: 'USD',nombre: 'Dolar de EU'},
+    {id: 'MXN',nombre: 'Peso Mexicano'},
+    {id: 'EUR',nombre: 'Euro'},
+    {id: 'GBP',nombre: 'Libra Esterlina'}
+]
+
+export {
+    monedas
+}
